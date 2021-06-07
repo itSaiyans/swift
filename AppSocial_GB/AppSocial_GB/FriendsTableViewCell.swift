e44f7411e65fa4d89d9a22c1f62d5ffd2e73cd76
itSaiyans <danilov.stanislav@outlook.com>
2021-06-07 20:58:07 +0300

    2ndLession

---
+++ AppSocial_GB/AppSocial_GB/FriendsTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FriendsTableView.swift
+//  AppSocial_GB
+//
+//  Created by Stas Danilov on 07.06.2021.
+//
+
+import UIKit
+
+final class FriendsTableViewCell: UITableViewCell {
+    static let identifier = "FriendsTableViewCell"
+}
